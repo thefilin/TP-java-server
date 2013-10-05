8b42a6d8b0202f56ee3265d3cdf9807b65f934b5
thefilin <filinsrespect@gmail.com>
2013-10-06 01:12:21 +0400

    Clear git

--- static/js/template.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $(".item").height(Math.max($(window).height()-60,586));
-});
-$(window).resize(function() {
-    $(".item").height(Math.max($(window).height()-60,586));
-});
