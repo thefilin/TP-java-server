d02228e83797bb7a11ae5203e884c9a16fe3a377
Stas <superbears@mail.ru>
2013-12-13 17:02:22 +0400

    Sendpage tests

--- src/test/java/frontend/HaveCookieTest.java
+++ src/test/java/frontend/HaveCookieTest.java
@@ -10,6 +10,8 @@ import messageSystem.MessageSystemImpl;
 
 import org.junit.Test;
 
+import dbService.UserDataSet;
+
 import base.MessageSystem;
 
 public class HaveCookieTest {
@@ -24,4 +26,18 @@ public class HaveCookieTest {
 		verify(mockedResponse).setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
 		verify(mockedResponse).addHeader("Location", "/");
 	}
+	
+	@Test
+	public void regAndHomeTest() {
+		
+		HttpServletResponse mockedResponse = mock(HttpServletResponse.class);
+		HaveCookie hc = new HaveCookie(new FrontendModel(new MessageSystemImpl()));
+		HaveCookie spyHc = spy(hc);
+		doNothing().when(spyHc).sendPage("reg.html", null, mockedResponse);
+		doNothing().when(spyHc).sendPage("index.html", null, mockedResponse);
+		spyHc.exec("/reg", null, null, null, mockedResponse, null);
+		spyHc.exec("/", null, null, null, mockedResponse, null);
+		verify(spyHc).sendPage("reg.html", null, mockedResponse);
+		verify(spyHc).sendPage("index.html", null, mockedResponse);
+	}	
 }
