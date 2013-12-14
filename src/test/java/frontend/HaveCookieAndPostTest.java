e6af45892a71bbd3c5aeb748f6fab2dcf37d2310
Stas <superbears@mail.ru>
2013-12-14 18:24:00 +0400

    HaveCookieAndPostTest

--- src/test/java/frontend/HaveCookieAndPostTest.java
+++ src/test/java/frontend/HaveCookieAndPostTest.java
@@ -11,22 +11,59 @@ import messageSystem.MessageSystemImpl;
 
 import org.junit.Test;
 
+import dbService.UserDataSet;
+
 import base.MessageSystem;
 
 public class HaveCookieAndPostTest {
+	
     @Test
-    public void responseTest()
+    public void userRequestTest()
     {
         HttpServletRequest mockedRequest = mock(HttpServletRequest.class);
         HttpServletResponse mockedResponse = mock(HttpServletResponse.class);
-        HaveCookieAndPost hcp = new HaveCookieAndPost(new FrontendModel(new MessageSystemImpl()));
-        String nick=mockedRequest.getParameter("what");
-        String password = mockedRequest.getParameter("islove");
-        hcp.exec("/test", null, null,mockedRequest , mockedResponse, null);
-        if ((nick==null)||(password==null)){
+        UserDataSet mockedUserSession = mock(UserDataSet.class);
+        HaveCookieAndPost hcp = new HaveCookieAndPost(mock(FrontendModel.class));
+        
+        when(mockedRequest.getParameter("nick")).thenReturn("testNick");
+        when(mockedRequest.getParameter("password")).thenReturn("testPass");
+        hcp.exec("/test", null, mockedUserSession,mockedRequest , mockedResponse, null);
         verify(mockedResponse).setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
         verify(mockedResponse).addHeader("Location", "/wait");
-        }
     }
+    
+    @Test
+    public void noUserRequestTest()
+    {
+        HttpServletRequest mockedRequest = mock(HttpServletRequest.class);
+        HttpServletResponse mockedResponse = mock(HttpServletResponse.class);
+        UserDataSet mockedUserSession = mock(UserDataSet.class);
+        HaveCookieAndPost hcp = new HaveCookieAndPost(mock(FrontendModel.class));
+        
+        when(mockedRequest.getParameter("regNick")).thenReturn("testNick");
+        when(mockedRequest.getParameter("regPassword")).thenReturn("testPass");
+        hcp.exec("/test", null, mockedUserSession,mockedRequest , mockedResponse, null);
+        verify(mockedResponse).setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+        verify(mockedResponse).addHeader("Location", "/wait");
+    }
+    
+    @Test
+    public void emptyUserTest()
+    {
+        HttpServletRequest mockedRequest = mock(HttpServletRequest.class);
+        HttpServletResponse mockedResponse = mock(HttpServletResponse.class);
+        UserDataSet mockedUserSession = mock(UserDataSet.class);
+        HaveCookieAndPost hcp = new HaveCookieAndPost(mock(FrontendModel.class)),
+        		hcpSpy = spy(hcp);
+        
+        doNothing().when(hcpSpy).sendPage("/test.html", null, mockedResponse);
+        when(mockedRequest.getParameter("regNick")).thenReturn("");
+        when(mockedRequest.getParameter("regPassword")).thenReturn("");
+        
+        hcpSpy.exec("/test", null, mockedUserSession,mockedRequest , mockedResponse, null);
+        verify(hcpSpy).sendPage("/test.html", mockedUserSession, mockedResponse);
+    }
+    
+    
 
 }
